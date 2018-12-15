d1a8df880bc07d2f03771dd016897635ff71f076
Josue Tchirktema <tchirktemajosue@gmail.com>
2018-12-15 20:29:09 +0000

    configuration de base pou l'acces a la base de donnee

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+   // parametre de connexion a BD;
+   $servername = 'localhost';
+   $database = 'e-gaz';
+   $username = 'root';
+   $password = '';
+
+   // creation d'une connexion a la base de donnee
+
+   $db = mysqli_connect($servername,$username,$password,$database);
+
+   // verification de l'etat de la connexion
+   if(!$db){
+     die('DB connexion failed : '. mysqli_connect_error());
+   } 
+
+ ?>
