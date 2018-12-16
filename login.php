a2fc414746d82050d7db399bf51f58c91039c886
Josue Tchirktema <tchirktemajosue@gmail.com>
2018-12-16 21:54:04 +0000

    design login form

---
+++ login.php
@@ -0,0 +1,1 @@
+<?php include 'views/login.views.php'; ?>
---
+++ views/login.views.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr" >
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <title>E-GAZ</title>
+  </head>
+  <body>
+    <ul>
+      <li>
+        <a class="active" href="#home">
+          <img src="assets/img/logo.png" alt="logo" srcset="">
+        </a>
+      </li>
+      
+    </ul>
+    <div class="login-page">
+      <h4>Je me connecte</h4>
+      <div class="form">
+        <form class="login-form" method="post">
+          <input type="text" placeholder="Email ou téléphone" name="username" id="username"/>
+          <input type="password" placeholder="mot de passe" name="password" id="password"/>
+          <button>Connexion</button>
+          <p class="message">Première visite ici ? <a href="#">créer un compte </a></p>
+        </form>
+      </div>
+    </div>
+
+
+  </body>
+</html>
